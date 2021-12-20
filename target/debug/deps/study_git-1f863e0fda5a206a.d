3f3d562786f781d1281727df8c25d068c2b9e019
forever <storchakvasil@gmail.com>
2021-12-20 12:42:03 +0200

    Four test commit

--- target/debug/deps/study_git-1f863e0fda5a206a.d
+++
@@ -1,5 +0,0 @@
-/home/forever/Projects/Another/study_git/target/debug/deps/study_git-1f863e0fda5a206a: src/main.rs
-
-/home/forever/Projects/Another/study_git/target/debug/deps/study_git-1f863e0fda5a206a.d: src/main.rs
-
-src/main.rs:
--- target/debug/study_git.d
+++
@@ -1,1 +0,0 @@
-/home/forever/Projects/Another/study_git/target/debug/study_git: /home/forever/Projects/Another/study_git/src/main.rs
