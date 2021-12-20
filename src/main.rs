03d92a55a40bb3adae822069bd6dc2eba3ff9f0b
forever <storchakvasil@gmail.com>
2021-12-20 15:41:02 +0200

    After merge

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     println!("Hello from test Rust project for study git!");
     my_mod::fn_from_my_mod();
     my_mod::my_mod::fn_from_my_mod_in_my_mod();
-    Command::new("python").arg("sp.py");
+    Command::new("python").arg("../sp.py");
 }
