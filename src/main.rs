acaa5365585126e3cff6841f8b6a639cc358a0a5
you <you@example.com>
2021-12-20 15:29:22 +0200

    Run py script

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::process::Command;
+
+
 mod my_mod;
+
 
 fn main() {
     println!("Hello from test Rust project for study git!");
     my_mod::fn_from_my_mod();
     my_mod::my_mod::fn_from_my_mod_in_my_mod();
+    Command::new("python").arg("sp.py");
 }
