fa95813c3801c79bac68e315df8a8d8745b43ea8
forever <storchakvasil@gmail.com>
2021-12-20 16:02:14 +0200

    Work all scripts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::io::{self, Write};
 
 mod my_mod;
 
@@ -6,5 +7,14 @@ fn main() {
     println!("Hello from test Rust project for study git!");
     my_mod::fn_from_my_mod();
     my_mod::my_mod::fn_from_my_mod_in_my_mod();
-    Command::new("python").arg("../sp.py");
+
+    let output_from_python = Command::new("python3")
+        .arg("src/sp.py")
+        .output()
+        .expect("Not run Python script");
+
+    println!("\nStatus code from python script: {}", output_from_python.status);
+
+    io::stdout().write_all(&output_from_python.stdout).unwrap();
+    io::stderr().write_all(&output_from_python.stderr).unwrap();
 }
