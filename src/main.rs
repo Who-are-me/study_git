4e6b56f2404bb7f1380214d76432caecdce31f85
forever <storchakvasil@gmail.com>
2021-12-20 12:17:02 +0200

    Test commit

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello from test Rust project for study git!");
+}
---
+++ target/debug/study_git.d
@@ -0,0 +1,1 @@
+/home/forever/Projects/Another/study_git/target/debug/study_git: /home/forever/Projects/Another/study_git/src/main.rs
